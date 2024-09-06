817a0b03f072a39e167949ffb4c6c0219ecd5f16
Fran Weidt <fran.weidt@gmail.com>
2024-09-06 18:47:27 -0300

    inital commit

---
+++ android/app/src/main/kotlin/com/example/weidtlab/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.weidtlab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'dart:math' as math;
+
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/palette.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'The Weidt Lab',
+            style: GoogleFonts.montserrat(color: Colors.black, fontSize: 24),
+          ),
+        ),
+        body: Center(
+          child: GameWidget(
+            game: FlameGame(world: MyWorld()),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyWorld extends World with TapCallbacks {
+  @override
+  Future<void> onLoad() async {
+    add(Square(Vector2.zero()));
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    super.onTapDown(event);
+    if (!event.handled) {
+      final touchPoint = event.localPosition;
+      add(Square(touchPoint));
+    }
+  }
+}
+
+class Square extends RectangleComponent with TapCallbacks {
+  static const speed = 3;
+  static const squareSize = 128.0;
+  static const indicatorSize = 6.0;
+
+  static final Paint red = BasicPalette.red.paint();
+  static final Paint blue = BasicPalette.blue.paint();
+
+  Square(Vector2 position)
+      : super(
+          position: position,
+          size: Vector2.all(squareSize),
+          anchor: Anchor.center,
+        );
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    add(
+      RectangleComponent(
+        size: Vector2.all(indicatorSize),
+        paint: blue,
+      ),
+    );
+    add(
+      RectangleComponent(
+        position: size / 2,
+        size: Vector2.all(indicatorSize),
+        anchor: Anchor.center,
+        paint: red,
+      ),
+    );
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    angle += speed * dt;
+    angle %= 2 * math.pi;
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    removeFromParent();
+    event.handled = true;
+  }
+}
